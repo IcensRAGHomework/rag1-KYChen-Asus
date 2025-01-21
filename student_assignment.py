d4d10dcdedaf3034e80226b69b06721134f516eb
yuan1_chen <Yuan1_Chen@asus.com>
2025-01-21 17:29:24 +0800

    hw

--- student_assignment.py
+++ student_assignment.py
@@ -248,6 +248,6 @@ def demo(question):
     return response
 
 
-# print(generate_hw01("2024年台灣10月紀念日有哪些?"))
-print(generate_hw03('2024年台灣10月紀念日有哪些?', '根據先前的節日清單，這個節日{"date": "10-31", "name": "蔣公誕辰紀念日"}是否有在該月份清單？'))
+print(generate_hw01("2024年台灣10月紀念日有哪些?"))
+# print(generate_hw03('2024年台灣10月紀念日有哪些?', '根據先前的節日清單，這個節日{"date": "10-31", "name": "蔣公誕辰紀念日"}是否有在該月份清單？'))
 # print(generate_hw04('請問中華台北的積分是多少'))
